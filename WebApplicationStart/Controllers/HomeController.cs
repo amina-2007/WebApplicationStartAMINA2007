5d316cf302bf4851d2a301d4fd17594f59be1e04
Алана <biragova.lyudmila@gmail.com>
2026-03-09 19:40:44 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Tasks()
+        {
+            return View();
+        }
+    }
+}
