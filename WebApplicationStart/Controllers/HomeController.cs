034c61e2b04a5ba270260ba59cc7f7914bda0f38
amina-2007 <aminagatagova@mail.ru>
2026-03-10 11:02:14 +0300

    gatagova task1 done

--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -16,5 +16,19 @@ namespace WebApplicationStart.Controllers
         {
             return View();
         }
+
+        public string Task1()
+        {
+            DateTime dateTime = DateTime.Now;
+            if (dateTime.Hour >= 0 && dateTime.Hour <= 5)
+            { return "ДОБРАЯ НОЧЬ"; }
+            if (dateTime.Hour >= 6 && dateTime.Hour <= 11)
+            { return "ДОБРОЕ УТРО"; }
+            if (dateTime.Hour >= 12 && dateTime.Hour <= 17)
+            { return "ДОБРЫЙ ДЕНЬ"; }
+            if (dateTime.Hour >= 18 && dateTime.Hour <= 23)
+            { return "ДОБРЫЙ ВЕЧЕР"; }
+            return "Тут решение первой задачи";
+        }
     }
 }
